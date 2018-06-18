0dbe5e5b23afc093e26a7d10e9cf44e54193554b
Carlos Villamar <cvillamar826@gmail.com>
2018-06-18 02:13:17 -0400

    server changes

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const PORT = process.env.PORT || 8080;
 
 // Create a server, uses `handleRequest` which is function that takes
 // care of providing requested data
-const server = http.createServer(handleRequest);
+//const server = http.createServer(handleRequest);
+
+var express = require('express')
+var server = express()
+
+server.get('/', (req,res)=>{
+  res.send("hello its me")
+})
 
 // Start the server
 server.listen(PORT, () => {
